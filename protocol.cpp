13fa176f0ffb8a6bd87d7da399603b67d3bb612b
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2025-08-19 19:07:35 +0200

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -24,7 +24,7 @@ static const char* ppszTypeName[] =
 
 unsigned short nDefaultP2Port = 0;
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xf2, 0xc2, 0xab, 0xd5 };
 
 CMessageHeader::CMessageHeader()
 {
